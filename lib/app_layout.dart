f00caba8003861345a35575ec4bac43cadc8e7c2
malcovys <malcovys22.aps@gmail.com>
2025-08-12 09:29:49 +0300

    Refactor app structure: create AppLayout widget for improved organization, update main entry point, and remove obsolete layout file.

---
+++ lib/app_layout.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sneakers/core/routes/app_router.dart';
+import 'package:sneakers/dependency_injection/injection.dart';
+import 'package:sneakers/presentation/managers/user/user_bloc.dart';
+import 'package:sneakers/presentation/managers/user/user_event.dart';
+
+class AppLayout extends StatelessWidget {
+  const AppLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    AppRouter appRouter = AppRouter();
+    return BlocProvider(
+      create: (context) => getIt<UserBloc>()..add(RetriveCurrentUserEvent()),
+      child: MaterialApp.router(
+          routerConfig: appRouter.config(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:sneakers/core/routes/app_router.dart';
+import 'package:sneakers/dependency_injection/injection.dart';
+import 'package:sneakers/app_layout.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await setupInjections();
   await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    AppRouter appRouter = AppRouter();
-    return MaterialApp.router(
-      routerConfig: appRouter.config(),
-    );
-  }
+  runApp(const AppLayout());
 }
