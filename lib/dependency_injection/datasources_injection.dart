43209129b9e51eaa93d5a08f88f5fe94b3d40f72
malcovys <malcovys22.aps@gmail.com>
2025-08-12 08:54:56 +0300

    Refactor dependency injection: reorganize datasource registration methods for improved clarity and structure.

--- lib/dependency_injection/datasources_injection.dart
+++ lib/dependency_injection/datasources_injection.dart
@@ -1,14 +1,31 @@
 part of './injection.dart';
 
 void _registerDatasources() {
-  _registerLocalDatasources();
-  _registerRemoteDatasources();
-}
+  _registerRemoteSourceServce();
+  _registerLocalService();
 
-void _registerLocalDatasources() {
   getIt.registerLazySingleton<UserLocalDatasource>(() => UserLocalDatasourceImpl(getIt()));
+  getIt.registerLazySingleton<UserRemoteDatasource>(() => UserRemoteDatasourceImpl(getIt()));
 }
 
-void _registerRemoteDatasources() {
+void _registerLocalService() {
+  getIt.registerLazySingletonAsync<LocalSource>(() async { 
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return Future.value(LocalSourceImpl(prefs));
+  });
+}
+
+void _registerRemoteSourceServce() {
+  getIt.registerLazySingleton<Dio>(() => Dio(
+    BaseOptions(
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json',
+      },
+      // baseUrl: 'https://api.example.com', // optional
+      // connectTimeout: const Duration(seconds: 10),
+      // receiveTimeout: const Duration(seconds: 10),
+    ),
+  ));
   getIt.registerLazySingleton<RemoteSource>(() => RemoteSourceImpl(getIt()));
 }
--- lib/dependency_injection/injection.dart
+++ lib/dependency_injection/injection.dart
@@ -1,13 +1,18 @@
-
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sneakers/application/usecases/user/login_usecase.dart';
 import 'package:sneakers/application/usecases/user/logout_usecase.dart';
 import 'package:sneakers/application/usecases/user/retrive_current_user_usecase.dart';
+import 'package:sneakers/core/sources/local/local_source.dart';
+import 'package:sneakers/core/sources/local/local_source_impl.dart';
 import 'package:sneakers/core/sources/remote/remote_source.dart';
 import 'package:sneakers/core/sources/remote/remote_source_impl.dart';
 import 'package:sneakers/domain/repositories/user_repository.dart';
 import 'package:sneakers/infrastructure/datasources/local/user/user_local_datasource.dart';
 import 'package:sneakers/infrastructure/datasources/local/user/user_local_datasource_impl.dart';
+import 'package:sneakers/infrastructure/datasources/remote/user/user_remote_datasource.dart';
+import 'package:sneakers/infrastructure/datasources/remote/user/user_remote_datasource_impl.dart';
 import 'package:sneakers/infrastructure/repositories/user_repository_impl.dart';
 import 'package:sneakers/presentation/managers/user/user_block.dart';
 
